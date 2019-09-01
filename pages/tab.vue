5c5f8006fed35ed2972636a758f422ead6018b54
yexuan <158521138@qq.com>
2019-09-01 15:55:26 +0800

    第一次上传

---
+++ pages/tab.vue
@@ -0,0 +1,53 @@
+<template>
+  <div class="tab">
+      <span 
+      v-for="(item,index) in ['体育','新闻','娱乐']" 
+      :key="index" 
+      @click="toggle(index)"
+      :class="{Red:current===index}"
+      >{{item}}</span>
+      
+      <ul v-show="current===0">
+          <li>体育</li>
+          <li>体育</li>
+          <li>体育</li>
+      </ul>
+      <ul v-show="current===1">
+          <li>新闻</li>
+          <li>新闻</li>
+          <li>新闻</li>
+      </ul>
+      <ul v-show="current===2">
+          <li>娱乐</li>
+          <li>娱乐</li>
+          <li>娱乐</li>
+      </ul>
+  </div>
+</template>
+
+<script>
+export default {
+    data(){
+        return{
+            current:0
+        }
+    },
+    methods:{
+        toggle(index){
+            this.current=index
+        }
+    }
+}
+</script>
+
+<style lang='less' scoped>
+    .tab{
+        margin:200px 100px;
+        span{
+            cursor: pointer;
+        }
+    }
+    .Red{
+        color: #f00;
+    }
+</style>
---
+++ store/pages/post/index.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+        旅途首页
+    </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>

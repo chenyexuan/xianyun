2527ebc20cd8d5085f7ddfe58b3ab70c76181de8
yexuan <158521138@qq.com>
2019-09-02 19:24:21 +0800

    公共页面及页脚完成

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,9 +1,20 @@
 <template>
   <div>
+    <Header></Header>
     <nuxt />
+    <Footer></Footer>
   </div>
 </template>
-
+<script>
+import Header from '@/components/header'
+import Footer from '@/components/footer'
+export default {
+  components:{
+    Header,
+    Footer
+  }
+}
+</script>
 <style lang="less">
 html {
   font-family: 'Source Sans Pro', -apple-system, BlinkMacSystemFont, 'Segoe UI',

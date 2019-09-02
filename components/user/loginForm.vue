fb83bac22cd47633eff4cfb686e7b2cdc2b69b9b
yexuan <158521138@qq.com>
2019-09-02 21:22:30 +0800

    首页跳转及登录页初步完成

---
+++ components/user/loginForm.vue
@@ -0,0 +1,69 @@
+<template>
+  <div class="login">
+    <el-form :model="loginForm" ref="loginForm" :rules="loginRules" class="form">
+      <el-form-item class="form-item" prop="username">
+        <el-input placeholder="用户名/手机" v-model="loginForm.username"></el-input>
+      </el-form-item>
+      <el-form-item class="form-item" prop="password">
+        <el-input placeholder="密码" type="password" v-model="loginForm.password"></el-input>
+      </el-form-item>
+      <p class="form-text">
+        <nuxt-link to="#">忘记密码</nuxt-link>
+      </p>
+      <el-button class="submit" type="success" @click="subLogin">登录</el-button>
+    </el-form>
+  </div>
+</template>
+
+<script>
+export default {
+  data(){
+    return{
+      loginForm:{
+        username:'',
+        password:''
+      },
+      loginRules:{
+        username:[
+          { required: true, message: '请输入用户名', trigger: 'blur' }
+        ],
+        password:[
+          { required: true, message: '请输入密码', trigger: 'blur' }
+        ]
+      }
+    }
+  },
+  methods:{
+    async subLogin(){
+      let res = await this.$axios({
+        url:'/accounts/login',
+        method:'post',
+        data:this.loginForm
+      })
+      console.log(res)
+    }
+  }
+};
+</script>
+
+<style lang='less' scoped>
+.form {
+  padding: 25px;
+}
+
+.form-item {
+  margin-bottom: 20px;
+}
+
+.form-text {
+  font-size: 12px;
+  color: #409eff;
+  text-align: right;
+  line-height: 1;
+}
+
+.submit {
+  width: 100%;
+  margin-top: 10px;
+}
+</style>
